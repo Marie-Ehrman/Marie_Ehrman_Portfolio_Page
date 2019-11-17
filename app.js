c253590eba145d6b6fc256052998c06f097758f3
Marie-Ehrman <53120243+Marie-Ehrman@users.noreply.github.com>
2019-11-17 06:58:45 -0800

    App.js
    Index and about routes working, handles 404 errors! Need to get project route working.

--- app.js
+++ app.js
@@ -1,43 +1,65 @@
-//initialize middleware variables
+//Add variables to require the necessary dependencies.
 const express = require('express');
 const app = express();
+//grab data from data.json file
+const { projects } = require('./data.json');
 
-const { data } = require('./data.json');
-
-
-//Add static assets
-app.use(express.static('public'));
-
-//Set 'view engine' to pug
+//set your “view engine” to “pug”
 app.set('view engine', 'pug');
 
-//establish route variables
-const index = require('./routes')
-const about = require('./routes/about');
+//set up static route
+app.use(express.static('public'));
 
-//Set up index route
-app.use(index);
-app.use(about);
 
-//start server
-app.listen(3000, () => {
-    console.log('Running on localhost:3000!')
+
+//****ROUTES****//
+
+/***** INDEX ROUTE *****/
+app.get('/', (req, res, next) => {
+    res.render('index', { projects });
 });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
-  });
-  
-  // error handler
-  app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-    // render the error page
-    res.status(err.status || 500);
+/***** ABOUT ROUTE *****/
+app.get('/about', (req, res, next) => {
+    res.render('about');
+});
+
+/***** PROJECT ROUTE *****/
+//Dynamic "project" routes (/project or /projects) based on the id of the project that render a 
+//customized version of the Pug project template to show off each project. Which means adding data, or "locals", as an object that contains data to be passed to the Pug template
+app.get('/project/:id', (req, res, next) => {
+    //stores the id with a route parameter
+    const projectId = req.params.id;
+    //holds the recipe object to pass to the view
+    const project = projects.find( ({ id }) => id === +projectId );
+    
+    if (project) {
+      res.render('project', { project });
+    } else {
+      res.sendStatus(404);
+    }
+});
+
+
+
+
+/**** ERROR HANDLING ****/
+
+//this handles a 404 not found error, it is placed here to catch other errors before the application ends
+app.use((req, res, next) => {
+    const err = new Error("Not Found!");
+    err.status = 404;
+    next(err);
+});
+
+//handles other errors and renders the error template
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
     res.render('error');
-  });
-  
-  module.exports = app;
+});
+
+
+app.listen(3000, () => {
+    console.log('Application running on localhost:3000!')
+});
