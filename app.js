caf1dc1c05be6f622d2a81a3603f8890163b16c7
Marie-Ehrman <53120243+Marie-Ehrman@users.noreply.github.com>
2019-11-20 14:36:35 -0800

    cleaning up the code

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Variables to require the necessary dependencies.
+//variables to require the necessary dependencies
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -9,24 +9,27 @@ const { projects } = require('./data.json');
 //set up Static Route to serve 'public' folder
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-//set “view engine” to “pug”
+//set “view engine” to pug
 app.set('view engine', 'pug');
 
     //**************ROUTES*****************//
 
 /***** INDEX ROUTE *****/
 app.get('/', (req, res, next) => {
+    //render the index pug template passing it the projects object
     res.render('index', { projects });
 });
 
 /***** ABOUT ROUTE *****/
 app.get('/about', (req, res, next) => {
+    //render the about pug template 
     res.render('about');
 });
 
 /***** PROJECT ROUTE *****/
 //Dynamic "project" routes based on the id of the project that render a 
-//customized version of the Pug project template to show off each project. Which means adding data, or "locals", as an object that contains data to be passed to the Pug template
+//customized version of the Pug project template to show off each project.
+//Which means adding data, or "locals", as an object that contains data to be passed to the Pug template
 app.get('/projects/:id', (req, res, next) => {
     
     //stores the id with a route parameter
@@ -37,11 +40,14 @@ app.get('/projects/:id', (req, res, next) => {
 
 
     if (project) {
+        //render the project pug template passing it the project object
       res.render('project', { project });
     } else {
+    //if route doesn't exist pass status 404 to 404 middleware
       res.sendStatus(404);
     }
 });
+
 
 /**** ERROR HANDLING ****/
 
@@ -52,7 +58,7 @@ app.use((req, res, next) => {
     next(err);
 });
 
-//handles other errors and renders the error template
+//handles other errors and renders the error pug template
 app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
@@ -60,6 +66,7 @@ app.use((err, req, res, next) => {
 });
 
 
+//run the local host server
 app.listen(3000, () => {
     console.log('Application running on localhost:3000!')
 });
